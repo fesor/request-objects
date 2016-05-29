f1c382d4fa86fad8df72059e9daf1d636d617cf0
Sergey Protko <fesors@gmail.com>
2016-05-30 00:17:31 +0300

    Fix violations list serialization

--- tests/examples/Request/ExtendedRegisterUserRequest.php
+++ tests/examples/Request/ExtendedRegisterUserRequest.php
@@ -4,6 +4,7 @@ namespace Fesor\RequestObject\Examples\Request;
 
 use Fesor\RequestObject\ErrorResponseProvider;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\ConstraintViolation;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 class ExtendedRegisterUserRequest extends RegisterUserRequest implements ErrorResponseProvider
@@ -12,7 +13,13 @@ class ExtendedRegisterUserRequest extends RegisterUserRequest implements ErrorRe
     {
         return new JsonResponse([
             'message' => 'Please check your data',
-            'errors' => $errors
+            'errors' => array_map(function (ConstraintViolation $violation) {
+
+                return [
+                    'path' => $violation->getPropertyPath(),
+                    'message' => $violation->getMessage()
+                ];
+            }, iterator_to_array($errors))
         ], 400);
     }
 
